71946cd1609a501b7227e8af19b71326b3ab9d2c
Maxime Buttard <maxime.buttard@gmail.com>
2018-05-21 11:02:27 -0400

    add input controller

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour {
+
+    void OnEnable() {
+        InputController.moveEvent += OnMoveEvent;
+        InputController.fireEvent += OnFireEvent;
+    }
+    void OnDisable() {
+        InputController.moveEvent -= OnMoveEvent;
+        InputController.fireEvent -= OnFireEvent;
+    }
+
+    private void OnFireEvent(object sender, InfoEventArgs<int> e) {
+        Debug.Log("Fire " + e.info);
+    }
+
+    private void OnMoveEvent(object sender, InfoEventArgs<Point> e) {
+        Debug.Log("Move " + e.info.ToString());
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
