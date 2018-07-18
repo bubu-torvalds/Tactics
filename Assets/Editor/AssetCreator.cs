a50aa42066f098609e41c01ac7355ee8be4ecaf3
Maxime Buttard <maxime.buttard@gmail.com>
2018-07-18 19:55:54 -0400

    Conversation

---
+++ Assets/Editor/AssetCreator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class AssetCreator  {
+
+    [MenuItem("Assets/Create/Conversation Data")]
+    public static void CreateConversationData() {
+        ScriptableObjectUtility.CreateAsset<ConversationData>();
+    }
+}
---
+++ Assets/Scripts/Model/ConversationData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationData : ScriptableObject {
+
+    public List<SpeakerData> list;
+
+}
